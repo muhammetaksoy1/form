9b1074ebafee28c8cda990d9d3649024abff1e34
= <1700003949@stu.iku.edu.tr>
2021-03-20 11:02:00 +0300

    I added content of the for.php file.

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php 
+
+    <form action="form.php" method="get">
+
+    <p>İsminiz: <input type="text" name="isim" /></p>
+
+    <p>Yaşınız: <input type="text" name="yaş" /></p>
+
+    <p><input type="submit" /></p>
+
+    </form>
+
+?>
