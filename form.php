7ee3bbb3c19a92077e6876d944a55e1d5e974617
= <1700003949@stu.iku.edu.tr>
2021-03-20 11:15:13 +0300

    I saw the key-value pair in the url.

--- form.php
+++ form.php
@@ -1,13 +1,26 @@
-<?php 
+<?php  <form action="form.php" method="get">
 
-    <form action="form.php" method="get">
+<p>İsminiz: <input type="text" name="isim" /></p>
 
-    <p>İsminiz: <input type="text" name="isim" /></p>
+<p>Yaşınız: <input type="text" name="yaş" /></p>
 
-    <p>Yaşınız: <input type="text" name="yaş" /></p>
+<p><input type="submit" /></p>
 
-    <p><input type="submit" /></p>
+</form> 
+ ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
 
-    </form>
 
-?>
+        Hoşgeldin <?php echo htmlspecialchars($_GET['isim']); ?>.
+         <?php echo (int)$_GET['yaş']; ?> yaşındasın.
+    </body>
+    </html>
+    
